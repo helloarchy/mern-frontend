6d528aa60bfc701acee9ef991ffadde887c54e74
Robert Hardy <helloarchy@gmail.com>
2020-09-02 18:25:47 +0100

    Add basic components for users and user lists

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = () => {
+    return (
+        <div>
+            User item works!
+        </div>
+    );
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import UserList from "../components/UsersList";
 
 const Users = () => {
-    return <h2>Users works!</h2>;
+    const USERS = [
+        {
+            id: 'u1',
+            image: 'https://www.onthisday.com/images/people/john-smith-medium.jpg',
+            name: 'John Smith',
+            places: 3
+        },
+        {
+            id: 'u2',
+            image: 'https://lezwatchtv.com/wp-content/uploads/2016/03/questionmarkface-350x412.jpg',
+            name: 'Jane Doe',
+            places: 5
+        }
+    ];
+
+    return <UserList items={USERS} />;
 };
 
 export default Users;
