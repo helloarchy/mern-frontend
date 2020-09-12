84bef5ac929a6dc7484eef68bdbd36f32af81e02
Robert Hardy <helloarchy@gmail.com>
2020-09-12 19:51:41 +0100

    Make side drawer a portal above root

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import reactDom from 'react-dom';
 import './SideDrawer.css'
 
+/**
+ * Send to portal to render above root
+ * @param props
+ * @returns {{children: *, implementation: *, containerInfo: *, $$typeof: (symbol|number), key: (null|string)}}
+ * @constructor
+ */
 const SideDrawer = props => {
-    return (
-        <aside className={'side-drawer'}>
-            {props.children}
-        </aside>
-    );
+    const content = <aside className={'side-drawer'}>
+        {props.children}
+    </aside>;
+
+    return reactDom.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
