79759cf047edd1245de54ff711ea52883c5ebf6c
Robert Hardy <helloarchy@gmail.com>
2020-10-07 16:05:10 +0100

    Extend Input to handle form validation

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import './Input.css';
 
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: true
+            };
+        default:
+            return state;
+    }
+};
+
 const Input = props => {
+    /* useReducer instead of state to handle interconnected states
+     * Can have second attribute for an initial state */
+    const [inputState, dispatch] = useReducer(inputReducer, {value: '', isValid: false});
+
+    const changeHandler = event => {
+        dispatch({type:'CHANGE', val:''});
+    };
+
     const element = props.element === 'input' ? (
-        <input id={props.id} type={props.type} placeholder={props.placeholder}/>
+        <input
+            id={props.id}
+            type={props.type}
+            placeholder={props.placeholder}
+            onChange={changeHandler}
+        />
     ) : (
-        <textarea id={props.id} rows={props.rows || 3}/>
+        <textarea
+            id={props.id}
+            rows={props.rows || 3}
+            onChange={changeHandler}
+        />
     );
+
     return (
         <div className={`form-control`}>
             <label htmlFor={props.id}>
