d8fdfb5c009eaebb517bafccd20d0ea9788cd62d
Robert Hardy <helloarchy@gmail.com>
2020-10-07 15:34:02 +0100

    Add input component for building up the new place form.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './NewPlace.css';
+import Input from "../../shared/components/FormElements/Input";
 
 const NewPlace = () => {
-    return <h2>New place works!</h2>
+    return (
+        <form className={'place-form'}>
+            <Input type={'text'} label={'Title'} element={'text'}/>
+        </form>
+    );
 };
 
 export default NewPlace;
